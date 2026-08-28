c09416830319ce27fbb77d244d5d594cf5c658da
Arnav Choudhary <arnavchoudhary2609@mountcarmeldelhi.com>
2026-08-28 19:01:44 +0530

    Connect chatbot to Render backend

--- frontend/app.js
+++ frontend/app.js
@@ -696,7 +696,7 @@ class ThinkSaathiApp {
     role: "user",
     content: text
 });
-    const response = await fetch("http://localhost:3000/chat", {
+    const response = await fetch("https://thinksaathinew.onrender.com/chat", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
